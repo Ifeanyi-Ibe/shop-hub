e560c5e1b5509db777efb02b73d1025e56e41531
Ibe Ifeanyi <ifeanyi-ibe@Ifeanyis-MacBook-Pro.local>
2024-07-12 14:54:17 +0100

    Initial setup

---
+++ src/ShopHub.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShopHub.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+        services.AddMediatR(configuration => 
+            configuration.RegisterServicesFromAssembly(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
